b526233d93b90ac537b8ec4be0badaec9ca6e51c
Xiomina Ramos <98005220+mina1707@users.noreply.github.com>
2022-04-06 07:56:55 -0700

    Add files via upload

---
+++ Human.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace HumanAssignment
+{
+    class Human
+    {
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+        
+        public int Health
+        {
+            get { return health; }
+        }
+        
+        public Human(string name)
+        {
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
+        
+        public Human(string name, int str, int intel, int dxt, int hp)
+        {
+            Name = name;
+            Strength = str;
+            Intelligence = intel;
+            Dexterity = dxt;
+            health = hp;
+        }
+        
+        // Build Attack method
+        public int Attack(Human target)
+        {
+            int dmg = Strength * 5;
+            target.health -= dmg;
+            Console.WriteLine($"{Name} attacked {target.Name} for {dmg} damage!");
+            return target.health;
+        }
+    }
+}
